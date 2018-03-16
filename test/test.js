2013399155180a64826fea9f6485c1f684645e71
Kay Lu <klu@stu.xidian.edu.cn>
2018-03-16 17:46:13 +0800

    fix bug

--- test/test.js
+++ test/test.js
@@ -10,16 +10,17 @@ let array1 = `[]`
 let array2 = `[{"a":1},{"b":"c"}]`
             
 let json4 = `{"a":{}}`
+let json5 = `{"a":"//"}`
 describe('parser', function() {
     // assert.equal()
     it('should equal {} -> []', function() {
         assert.equal(JSON.stringify(parser.parser(json1)), '[]')
     });
     it('should equal {"a":"b"}', function() {
-        assert.equal(JSON.stringify(parser.parser(json2)), '[{"key":"a","children":"\\"b\\"","value":"\\"b\\"","type":"string"}]')
+        assert.equal(JSON.stringify(parser.parser(json2)), '[{"key":"a","children":[],"value":"\\"b\\"","type":"string"}]')
     });
     it('should equal {"a":"b"//comment}', function() {
-        assert.equal(JSON.stringify(parser.parser(json3)), '[{"key":"a","children":"\\"b\\"","value":"\\"b\\"","type":"string","description":"comment"}]')
+        assert.equal(JSON.stringify(parser.parser(json3)), '[{"key":"a","children":[],"value":"\\"b\\"","type":"string","description":"comment"}]')
     });
     it('should equal {"a":"b"}', function() {
         assert.equal(JSON.stringify(parser.parser(json4)), '[{"key":"a","children":[],"value":"{}","type":"object"}]')
@@ -28,6 +29,9 @@ describe('parser', function() {
         assert.equal(JSON.stringify(parser.parser(array1)), '[]')
     })
     it('shoult equal', () => {
-        assert.equal(JSON.stringify(parser.parser(array2)), '[[{"key":"a","children":"1","value":"1","type":"number"}],[{"key":"b","children":"\\"c\\"","value":"\\"c\\"","type":"string"}]]')
+        assert.equal(JSON.stringify(parser.parser(array2)), '[[{"key":"a","children":[],"value":"1","type":"number"}],[{"key":"b","children":[],"value":"\\"c\\"","type":"string"}]]')
+    })
+    it('shoult equal', () => {
+        assert.equal(JSON.stringify(parser.parser(json5)), '[{"key":"a","children":[],"value":"\\"//\\"","type":"string"}]')
     })
 })
